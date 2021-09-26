dd837f324c9c9e3226660f1180cb7428734bcf91
Rafahdevc <91375149+Rafahdevc@users.noreply.github.com>
2021-09-26 11:48:58 -0300

    Add files via upload

---
+++ Atividade 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main(){
+	
+	int numero = 0;
+	
+	std::cout << "Digite um numero: ";
+	std::cin >> numero;
+	
+	if(numero > 20){
+		std::cout << numero;
+	
+	}else if(numero = 20){
+		std::cout << "Esta chegando perto";
+	}
+	
+	
+	return 0;
+}
