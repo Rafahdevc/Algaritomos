fe0214550b6a53d8a5574295bceef6ce7eaf460b
Rafahdevc <91375149+Rafahdevc@users.noreply.github.com>
2021-09-26 11:50:30 -0300

    Add files via upload

---
+++ Atividade 3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main(){
+	
+	int numero = 0;
+	
+	std::cout << "Digite o numero: ";
+	std::cin >> numero;
+	
+	if((numero % 2) == 0){
+   
+   std::cout << numero;
+   
+}
+	
+	return 0;
+}
